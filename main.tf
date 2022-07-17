7b2cb32f66153dffaf99685983a7bee47c61c366
santhosh <sk250600@ncr.com>
2022-07-17 17:58:45 +0530

    code changes

--- main.tf
+++ main.tf
@@ -120,4 +120,19 @@ resource "aws_instance" "web-1" {
 	CostCenter = "ABCD"
     }
 }
+resource "aws_instance" "web-2" {
+    ami = var.imagename
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "AWSKey"
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    tags = {
+        Name = "Prod-Server-2"
+        Env = "Prod"
+        Owner = "Santhosh"
+	CostCenter = "ABCD"
+    }
+}
 
