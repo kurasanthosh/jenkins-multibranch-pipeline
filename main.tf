d5f835fa8720e156244abe4b722f88f54fef44c9
santhosh <sk250600@ncr.com>
2022-07-17 18:01:09 +0530

    code changes

--- main.tf
+++ main.tf
@@ -115,7 +115,22 @@ resource "aws_instance" "web-1" {
     associate_public_ip_address = true	
     tags = {
         Name = "UAT-Server-1"
-        Env = "UAT-server"
+        Env = "UAT"
+        Owner = "Santhosh"
+	CostCenter = "ABCD"
+    }
+}
+resource "aws_instance" "web-2" {
+    ami = var.imagename
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "AWSKey"
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    tags = {
+        Name = "UAT-Server-2"
+        Env = "UAT"
         Owner = "Santhosh"
 	CostCenter = "ABCD"
     }
