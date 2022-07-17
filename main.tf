43d7cf7a1af2c57ce67d8adf8ede31029437667a
santhosh <sk250600@ncr.com>
2022-07-17 18:11:53 +0530

    code changes

--- main.tf
+++ main.tf
@@ -135,4 +135,19 @@ resource "aws_instance" "web-2" {
 	CostCenter = "ABCD"
     }
 }
+resource "aws_instance" "web-" {
+    ami = var.imagename
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "AWSKey"
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    tags = {
+        Name = "UAT-Server-3"
+        Env = "UAT"
+        Owner = "Santhosh"
+	CostCenter = "ABCD"
+    }
+}
 
